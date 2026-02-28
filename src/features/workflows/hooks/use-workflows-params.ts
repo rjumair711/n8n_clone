d2b1c37c3caa1d80ec2ac854cba42dc2106e0501
RJ Umair <umairajput711@gmail.com>
2026-02-28 15:46:35 +0500

    workflow-pagination

---
+++ src/features/workflows/hooks/use-workflows-params.ts
@@ -0,0 +1,10 @@
+import { workflowsParams } from './../params';
+import { useQueryStates } from "nuqs"
+
+
+
+export const useWorkflowsParams = () => {
+    return useQueryStates(workflowsParams)
+}
+
+
---
+++ src/features/workflows/server/params-loader.ts
@@ -0,0 +1,5 @@
+import { workflowsParams } from './../params';
+import { createLoader } from "nuqs/server"
+
+
+export const workflowsParamsLoader = createLoader(workflowsParams)
